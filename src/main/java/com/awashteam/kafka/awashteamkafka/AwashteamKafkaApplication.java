231880bf8e58fea5d28c5f4e12566ced66ae7836
awashteam <awashteam@gmail.com>
2020-10-01 14:04:16 -0500

    Commit First kafka tutorial.

---
+++ src/main/java/com/awashteam/kafka/awashteamkafka/AwashteamKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.awashteam.kafka.awashteamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwashteamKafkaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AwashteamKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/awashteam/kafka/awashteamkafka/AwashteamKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awashteam.kafka.awashteamkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwashteamKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
